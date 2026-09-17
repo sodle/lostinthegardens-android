8c602c9262fe4c5417d0cce5ae232346fb2d77b2
Scott Odle <scott@sjodle.com>
2026-09-17 15:54:26 -0600

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sjodle.lostinthegardens"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
